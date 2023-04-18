5dee0263fc1f7943cb789d80373b5089642e496d
Daniel O'Hanlon <dohanlon@ebi.ac.uk>
2023-04-18 13:48:49 +0100

    Update entry script

--- valinor/valinor.py
+++ valinor/valinor.py
@@ -1,12 +1,63 @@
 import argparse
 
-def prepareData(data_files, only_singletons = False, no_singletons = False, no controls = True):
+from models import valinorHierarchy
+from utils import getIndices, calculateLengths
 
+
+def prepareData(
+    data_files, only_singletons=False, no_singletons=False, no_controls=True
+):
     pass
 
-def runValinor(lengths, indices, prior_params, data, name = '', no_singletons=False, only_singletons=False, no_controls = False):
 
-    pass
+def runValinor(
+    lengths,
+    indices,
+    prior_params,
+    data,
+    name="",
+    no_singletons=False,
+    only_singletons=False,
+    no_controls=False,
+):
+
+    guide = AutoNormal(valinorHierarchy)
+
+    optimizer = numpyro.optim.Adam(step_size=config['lr'])
+
+    svi = SVI(model, guide, optimizer, loss=TraceMeanField_ELBO(num_particles=1))
+
+    svi_result = svi.run(
+        random.PRNGKey(42),
+        config['epochs'],
+        data,
+        lengths,
+        indices,
+        prior_params,
+        no_singletons=config['no_singletons'],
+        only_singletons=config['only_singletons'],
+        no_controls=config['no_controls'],
+        stable_update=config['stable_update'],
+    )
+
+    params = svi_result.params
+
+    saveModelParams(params, config['paramsFileName'])
+
+    predictive = Predictive(guide, params=params, num_samples=config['nSamples'])
+
+    # Sample from posterior
+    samples = predictive(
+        random.PRNGKey(42),
+        data,
+        lengths,
+        indices,
+        prior_params,
+        no_singletons=config['no_singletons'],
+        only_singletons=config['only_singletons'],
+        no_controls=config['no_controls'],
+    )
+
 
 if __name__ == "__main__":
 
@@ -39,10 +90,23 @@ if __name__ == "__main__":
 
     argParser.add_argument("-n", type=str, dest="name", default="", help="Output name.")
 
+    argParser.add_argument(
+        "--config",
+        "-c",
+        type=str,
+        dest="config",
+        default=None,
+        help="Config JSON file (overrides all other config)",
+    )
+
     args = argParser.parse_args()
 
     lengths, indices, prior_params, data = prepareData(args.only_singletons)
 
     runValinor(
-        lengths, indices, prior_params, data, args.name, args.no_singletons, args.only_singletons, args.no_controls
+        lengths,
+        indices,
+        prior_params,
+        data,
+        config
     )
