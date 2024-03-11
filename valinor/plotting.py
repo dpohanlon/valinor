d9e3aacb91dc43ff7d7a1a81a0d1dbcd9cb72881
Daniel O'Hanlon <dohanlon@ebi.ac.uk>
2024-03-11 15:31:08 +0000

    Add output directory CLI specification.

--- valinor/plotting.py
+++ valinor/plotting.py
@@ -16,13 +16,17 @@ rcParams.update({"figure.autolayout": True})
 import numpy as np
 
 
-def plotLossCurve(loss, log=True, name=None):
+def plotLossCurve(loss, log=True, name=None, outputDir=""):
     plt.plot(np.log(loss))
     plt.ylabel("ELBO")
     plt.xlabel("Steps")
-    plt.savefig("valinor_loss.pdf" if name == None else f"valinor_loss_{name}.pdf")
+    plt.savefig(
+        f"{outputDir}valinor_loss.pdf"
+        if name == None
+        else f"{outputDir}valinor_loss_{name}.pdf"
+    )
     plt.clf()
 
 
-def plotDiagPlots(svi_result, name=None):
-    plotLossCurve(svi_result.losses, name=name)
+def plotDiagPlots(svi_result, name=None, outputDir=""):
+    plotLossCurve(svi_result.losses, name=name, outputDir=outputDir)
