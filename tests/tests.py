cf407943bdc6ea3590cea965c08ce323816b1c93
Daniel O'Hanlon <dohanlon@ebi.ac.uk>
2024-04-04 11:46:14 +0100

    Clean up test config.

--- tests/tests.py
+++ tests/tests.py
@@ -9,52 +9,12 @@ import unittest
 import os
 
 
-# def testWithCLI():
-#     # cwd: valinor
-
-#     args = "--combinationsFile tests/data/dfCombs_sim.pq "
-#     args += "--singletonsFile tests/data/dfSgl_sim.pq "
-#     args += "--controlsFile tests/data/dfCalib_sim.pq "
-#     args += "--epochs 10 "
-#     args += "--nSamples 10 "
-#     args += "-n cliTest "
-
-#     sp.check_call(f"valinor {args}", shell=True)
-
-#     args = "--combinationsFile tests/data/dfCombs_sim.pq "
-#     args += "--singletonsFile tests/data/dfSgl_sim.pq "
-#     args += "--controlsFile tests/data/dfCalib_sim.pq "
-#     args += "--epochs 10 "
-#     args += "--nSamples 10 "
-#     args += "-n cliTest "
-#     args += "--priorsFile tests/priors.yaml "
-
-#     sp.check_call(f"valinor {args}", shell=True)
-
-# def testWithCLIReport():
-#     # cwd: valinor
-
-
-# def testGenerateData():
-#     # Call the simulator to generate some data for our tests
-
-#     if not os.path.isdir("tests/data"):
-#         os.mkdir("tests/data")
-
-#     args = "--out-dir tests/data "
-
-
-#     sp.check_call(f"valinor_sim {args}", shell=True)
-
-
-class Test01ValinorSim(unittest.TestCase):
+class TestValinorSim(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print("Setting up simulation data test class...")
 
     def testGenerateData(self):
-        # Call the simulator to generate some data for our tests
-
         if not os.path.isdir("tests/data"):
             os.mkdir("tests/data")
 
@@ -63,10 +23,9 @@ class Test01ValinorSim(unittest.TestCase):
         sp.check_call(f"valinor_sim {args}", shell=True)
 
 
-class Test02Valinor(unittest.TestCase):
+class TestValinor(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # This method runs once before the first test of this class
         print("Setting up Valinor test class...")
 
     def testWithCLI(self):
@@ -95,10 +54,9 @@ class Test02Valinor(unittest.TestCase):
         sp.check_call(f"valinor {args}", shell=True)
 
 
-class Test03ValinorReport(unittest.TestCase):
+class TestValinorReport(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # This method runs once before the first test of this class
         print("Setting up Valinor test REPORT class...")
 
     def testWithCLIReport(self):
@@ -118,27 +76,8 @@ class Test03ValinorReport(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    # unittest test runner will automatically find all classes that inherit from unittest.TestCase in the module
     suite = unittest.TestSuite()
-    for test_class in [Test01ValinorSim, Test02Valinor, Test03ValinorReport]:
+    for test_class in [TestValinorSim, TestValinor, TestValinorReport]:
         tests = unittest.defaultTestLoader.loadTestsFromTestCase(test_class)
         suite.addTests(tests)
     unittest.TextTestRunner().run(suite)
-    # argParser = argparse.ArgumentParser()
-
-    # argParser.add_argument(
-    #     "--generate-input",
-    #     action="store_true",
-    #     dest="generate_input",
-    #     default=False,
-    #     help="Generate a simulated input dataset for integration testing.",
-    # )
-
-    # args = argParser.parse_args()
-
-    # if args.generate_input:
-    #     testGenerateData()
-
-    # else:
-    #     testWithCLI()
-    #     testWithCLIPriors()
